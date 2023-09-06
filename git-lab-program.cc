69c13f205d6bcade0ecfccdee8be794ae35c06f3
wasay7861 <wahmed526722@ohio.edu>
2023-09-06 18:40:47 -0400

    HELLLo

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+*       File : git-lab-program.cc
+*       Author: Wasay AHmed
+*       Date: 9/6/2023
+*       Decription: Prints Hello
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
